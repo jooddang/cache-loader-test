da066e47d98d355261aacf128c12e71d7bd7c8a6
eunkwang <eunkwang@nextdoor.com>
2017-09-06 12:09:45 -0700

    cache loader setting

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
+const path = require('path');
+
 module.exports = () => {
 	return [
 		{
 			entry: {'test': 'test.js'} ,
 			resolve: { modules: ['node_modules', 'src'], extensions: ['.js'] },
+			module: {rules: [{test: /\.js$/, use: [{loader: 'cache-loader', options: {cacheDirectory: path.resolve('node_modules/.cache-loader-cache')}}, 'babel-loader']}]},
 			output: {
 				filename: '[name]_built.js'
 			}
